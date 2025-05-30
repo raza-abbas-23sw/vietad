0e399d94c47c81c510a16468ca3c34817c20e6e8
raza-abbas-23sw <raza84abbas@gmail.com>
2025-05-30 16:32:05 +0500

    installed and setup tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Lets begin the Journey boyss!</h1>
+      <h1 className="flex items-center justify-center h-full bg-green-300 text-5xl font-bold">
+        Lets begin the Journey boyss!
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
