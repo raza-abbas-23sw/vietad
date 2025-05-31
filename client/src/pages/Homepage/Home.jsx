7c55040d264752796c5e0b8abc923cabacfdc4c5
abdul majid <majidalitangri7@gmail.com>
2025-05-31 15:45:56 +0500

    Added-Custom-sign-inThe-homepage

--- client/src/pages/Homepage/Home.jsx
+++ client/src/pages/Homepage/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from './HeroSection' // Make sure the path is correct based on your folder structure
-
+import CustomSignSection from './CustomSignSection'
 const Home = () => {
   return (
     <div>
       <HeroSection />
+            <CustomSignSection />
+
     </div>
   )
 }
