84f1eb9247e462dc3155a65b239a7cfc2d986aef
SirajAnsari3 <siraj2022ansari@gmail.com>
2025-06-03 12:05:53 +0500

    fib

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -8,14 +8,17 @@
 
 
 // Import Firebase Admin SDK
-import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+// firebase.js
+import { initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
-import serviceAccount from './firebaseServiceKey.json' assert { type: 'json' };
+import { createRequire } from 'module';
 
-// Initialize app if not already initialized
+const require = createRequire(import.meta.url);
+const serviceAccount = require('./firebaseServiceKey.json');
+
 const adminApp = initializeApp({
   credential: cert(serviceAccount),
 });
 
-// Export auth only (or export adminApp too if needed)
 export const adminAuth = getAuth(adminApp);
+
